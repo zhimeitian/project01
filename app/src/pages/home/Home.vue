f67bc7cf01b29ab2cfe181d421d2c9f9c8dab4fa
zhimeitian <787809822@qq.com>
2022-10-08 16:05:16 +0800

    e-chart的引入

--- app/src/pages/home/Home.vue
+++ app/src/pages/home/Home.vue
@@ -47,12 +47,44 @@
 </template>
 
 <script>
+import * as echarts from 'echarts';
+
 export default {
-name:'Home'
+name:'Home',
+methods:{
+
+    initCurve(){
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('curve_show'));
+// 绘制图表
+myChart.setOption({
+  title: {
+    text: 'ECharts 入门示例'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
+    }
+
+},
+// 真实dom挂载完成后才能获得元素进行操作
+mounted(){
+  this.initCurve() // 绘制图表
+}
 }
 </script>
 
-<style>
+<style scoped>
 .spannel_list {
   margin-top: 20px;
 }
